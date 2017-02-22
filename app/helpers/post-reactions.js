b26922bf4c779c09a6c4734acb1d1c78a5df345f
Márcio Passos <marciopassosbel@gmail.com>
2017-02-22 01:51:27 -0300

    reactions

--- app/helpers/post-reactions.js
+++ app/helpers/post-reactions.js
@@ -7,40 +7,105 @@ export function postReactions([reactions]) {
    let wow = 0;
    let haha = 0;
    let angry = 0;
+   let sad = 0;
    let thankful = 0;
    // debugger;
-   reactions.map(function(reaction) {
-   	console.log(reaction.type);
-      switch (reaction.type) {
-   		case 'LIKE':
-   			like += 1;
-   			break;
-   		case 'LOVE':
-   			love += 1;
-   			break;
-   		case 'WOW':
-   			wow += 1;
-   			break;
-   		case 'HAHA':
-   			haha += 1;
-   			break;
-   		case 'ANGRY':
-   			angry += 1;
-   			break;
-   		case 'THANKFUL':
-   			thankful += 1;
-   			break;
-    }
-   });
-   let out = '';
-   out +=  '<li><i class="fa fa-thumbs-up"></i> ' + like + '</li>';
-   out +=  '<li><i class="fa fa-heart"></i> ' + love + '</li>';
-   out +=  '<li><i class="fa fa-heart"></i> ' + wow + '</li>';
-   out +=  '<li><i class="fa fa-heart"></i> ' + haha + '</li>';
-   out +=  '<li><i class="fa fa-heart"></i> ' + angry + '</li>';
-   out +=  '<li><i class="fa fa-heart"></i> ' + thankful + '</li>';
+   function getJSON(url) {
+     return new Promise(function(resolve, reject){
+       var xhr = new XMLHttpRequest();
+
+       xhr.open('GET', url);
+       xhr.onreadystatechange = handler;
+       xhr.responseType = 'json';
+       xhr.setRequestHeader('Accept', 'application/json');
+       xhr.send();
+
+       function handler() {
+         if (this.readyState === this.DONE) {
+           if (this.status === 200) {
+             resolve(this.response);
+           } else {
+             reject(new Error('getJSON: `' + url + '` failed with status: [' + this.status + ']'));
+           }
+         }
+       };
+     });
+   }
+   var out = '';
+   function countReactions(reactions, out) {
+      // let out = '';
+      reactions.data.map(function(reaction) {
+         switch (reaction.type) {
+            case 'LIKE':
+               like += 1;
+               break;
+            case 'LOVE':
+               love += 1;
+               break;
+            case 'HAHA':
+               haha += 1;
+               break;
+            case 'WOW':
+               wow += 1;
+               break;
+            case 'SAD':
+               sad += 1;
+               break;
+            case 'ANGRY':
+               angry += 1;
+               break;
+            case 'THANKFUL':
+               thankful += 1;
+               break;
+         }
+      });
+
+      out +=  '<li><img src="/assets/images/facebook-like.png" alt="..." class="fb-reactions"> ' + like + '</li>';
+      out +=  '<li><img src="/assets/images/facebook-love.png" alt="..." class="fb-reactions"> ' + love + '</li>';
+      out +=  '<li><img src="/assets/images/facebook-haha.png" alt="..." class="fb-reactions"> ' + haha + '</li>';
+      out +=  '<li><img src="/assets/images/facebook-wow.png" alt="..." class="fb-reactions"> ' + wow + '</li>';
+      out +=  '<li><img src="/assets/images/facebook-sad.png" alt="..." class="fb-reactions"> ' + sad + '</li>';
+      out +=  '<li><img src="/assets/images/facebook-angry.png" alt="..." class="fb-reactions"> ' + angry + '</li>';
+      out +=  '<li><i class="fa fa-leaf"></i> ' + thankful + '</li>';
+         
+      if (reactions.paging.next) {
+         console.log('tem mais reactions');
+         // $.get(reactions.paging.next).then(function(data) {
+         //    console.log('paginating...');
+         //    countReactions(data);
+         // });
+         getJSON(reactions.paging.next).then(function(data) {
+            // on fulfillment
+            console.log('paginating...');
+            countReactions(data, out);
+         }, function(reason) {
+            // on rejection
+            console.log('page bug...');
+            // countReactions(data);
+         });
+      } else {
+         console.log('acabou paginação', out);
+         return out;
+      }
+   }
+   // let saida = countReactions(reactions, out);
+   
    // out += '</ul>';
-   return Ember.String.htmlSafe(out);
+   var saida = new Promise(function(resolve, reject) {
+     // on success
+     let value = countReactions(reactions, '');
+     resolve(value);
+
+     // on failure
+     reject(new Error('DEU BUG NA REACION'));
+   });
+   return saida.then(function(data) {
+      // debugger;
+      return Ember.String.htmlSafe(data);
+   });
+
+   // return Ember.String.htmlSafe(saida);
+   
 }
 
 export default Ember.Helper.helper(postReactions);
---
+++ app/helpers/post-shares.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export function postShares([value]) {
+  
+  return Ember.String.htmlSafe(total);
+}
+
+export default Ember.Helper.helper(postShares);
