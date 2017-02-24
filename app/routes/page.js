bc4a87b1523afda50e289205579d635ed6e31742
Márcio Passos <marciopassosbel@gmail.com>
2017-02-24 12:03:14 -0300

    promise reactions

--- app/routes/page.js
+++ app/routes/page.js
@@ -9,57 +9,41 @@ export default Ember.Route.extend({
 		var _this = this;
 		let count = 0;
 		var getJSON = this.get('getJSON');
-		// var jsonItem = '';
-		// debugger;
-		var jsonItem = $.get(`http://www.instadev.com.br/facebook-api-wrapper/page`, {
-			access_token: tk,
-			id: id
-		}).then(item => {
-			let json = JSON.parse(item);
-			// jsonItem.posts.data.map(post=>{
-			json.posts.data.map(post=>{ 
-				post.reactions_new = getJSON(post.id, tk).then(data=> {
-					
-					return data;
-				});
-				// $.get(`http://www.instadev.com.br/facebook-api-wrapper/post_reactions_count`, {
-		  //       	access_token: tk,
-		  //       	id: post.id
-		  //     	}).then(reactions=> {
-		  //     		return reactions;
-		  //     	});
-		    });
-			// });
-			// debugger;
-			return json;
-		});
-		return jsonItem;
+		return Ember.RSVP.hash({
+	        page: $.get(`http://www.instadev.com.br/facebook-api-wrapper/page`, {
+				access_token: tk,
+				id: id
+			}).then(item => {
+				let json = JSON.parse(item);
+				// getReactions(json);
+				return json;
+		    }),
+		    reactions: $.get(`http://www.instadev.com.br/facebook-api-wrapper/page`, {
+				access_token: tk,
+				id: id
+			}).then(item => {
+				let json = JSON.parse(item);
+				return json.posts.data.map(post=>{ 
+					return getJSON(post.id, tk);//then(data=> {
+					// 	return data;
+					// });
+		    	});
+		    })
+		});	
 	},
 	getJSON: function(id, tk) {
-     // return new Promise(function(resolve, reject){
-
-      return $.get(`http://www.instadev.com.br/facebook-api-wrapper/post_reactions_count`, {
-        access_token: tk,
-        id: id
-      }).then(item => {
-        let jsonItem = JSON.parse(item);
-        // console.log('jsonItemReactions', jsonItem.like.summary.total_count);
-        // let response =  Ember.String.htmlSafe('Likes: ' + jsonItem.like.summary.total_count);
-        console.log('response', jsonItem);
-        // return response;
-        // resolve(jsonItem);
-        return jsonItem;
-      }, function() {
-        reject(new Error('getJSON: `' + url + '` failed with status: [' + this.status + ']'));
-        // return "bug";
-      });
-
-     // });
-    },
-	// setupController(controller) {
-	// 	this._super(...arguments);
-	// 	controller.set('getpromise', this.get('getpromise'));
-	// }
+	    return new Promise(function(resolve, reject){
+		    $.get(`http://www.instadev.com.br/facebook-api-wrapper/post_reactions_count`, {
+		        access_token: tk,
+		        id: id
+		    }).then(item => {
+		        let jsonItem = JSON.parse(item);
+		        console.log('response', jsonItem);
+		        resolve(jsonItem);
+		        // return jsonItem;
+		    }, function() {
+		        reject(new Error('getJSON: `' + url + '` failed with status: [' + this.status + ']'));
+		    });
+	    });
+	}
 });
-
-// this.get('page').cover.source;
