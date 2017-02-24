b04f074084bf9aa0d1235a5f4bdfd2fb90d24f14
Márcio Passos <marciopassosbel@gmail.com>
2017-02-24 02:35:44 -0300

    reactions...

--- app/routes/page.js
+++ app/routes/page.js
@@ -1,20 +1,65 @@
 import Ember from 'ember';
 
+var PromiseObject = Ember.ObjectProxy.extend(Ember.PromiseProxyMixin);
+
 export default Ember.Route.extend({
 	model(params) {
-		let tk  = this.get('session.accessToken');
+		var tk  = this.get('session.accessToken');
 		let id = params.pageid;
-		let _this = this;
+		var _this = this;
 		let count = 0;
-		return $.get(`http://www.instadev.com.br/facebook-api-wrapper/page`, {
+		var getJSON = this.get('getJSON');
+		// var jsonItem = '';
+		// debugger;
+		var jsonItem = $.get(`http://www.instadev.com.br/facebook-api-wrapper/page`, {
 			access_token: tk,
 			id: id
 		}).then(item => {
-			let jsonItem = JSON.parse(item);
-			console.log('jsonItemPage', jsonItem);
-			return jsonItem;
+			let json = JSON.parse(item);
+			// jsonItem.posts.data.map(post=>{
+			json.posts.data.map(post=>{ 
+				post.reactions_new = getJSON(post.id, tk).then(data=> {
+					
+					return data;
+				});
+				// $.get(`http://www.instadev.com.br/facebook-api-wrapper/post_reactions_count`, {
+		  //       	access_token: tk,
+		  //       	id: post.id
+		  //     	}).then(reactions=> {
+		  //     		return reactions;
+		  //     	});
+		    });
+			// });
+			// debugger;
+			return json;
 		});
-	}
+		return jsonItem;
+	},
+	getJSON: function(id, tk) {
+     // return new Promise(function(resolve, reject){
+
+      return $.get(`http://www.instadev.com.br/facebook-api-wrapper/post_reactions_count`, {
+        access_token: tk,
+        id: id
+      }).then(item => {
+        let jsonItem = JSON.parse(item);
+        // console.log('jsonItemReactions', jsonItem.like.summary.total_count);
+        // let response =  Ember.String.htmlSafe('Likes: ' + jsonItem.like.summary.total_count);
+        console.log('response', jsonItem);
+        // return response;
+        // resolve(jsonItem);
+        return jsonItem;
+      }, function() {
+        reject(new Error('getJSON: `' + url + '` failed with status: [' + this.status + ']'));
+        // return "bug";
+      });
+
+     // });
+    },
+	// setupController(controller) {
+	// 	this._super(...arguments);
+	// 	controller.set('getpromise', this.get('getpromise'));
+	// }
 });
 
 // this.get('page').cover.source;
